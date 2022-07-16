be4c1374e934640264ae8d7db6bd81be0d376d2d
unknown <{sanathan6522@gmail.com}>
2022-07-15 16:28:19 -0800

    fixed the text

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,3 @@
 export default function AllMeetupsPage(){
-    return <div>All Meetups Page</div>;
+    return <div className="center">All Meetups Page</div>;
 }
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,3 +1,3 @@
 export default function FavouritesPage(){
-    return <div>Favourites Page</div>;
+    return <div className="center">Favourites Page</div>;
 }
